0d672ed29890b7a39693a80b6381c6709ef58f99
lsahoo <lokanath.sahoo@factset.com>
2023-03-10 15:18:28 +0530

    testing aws features

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const app = express();
 
 app.use(express.json())
 
-app.get('/something', (req, res) => {
+app.get('/', (req, res) => {
     res.json({
         "message": "Hello from the cloud"
     })
 })
 
 app.listen(8001, () => {
-    console.log("server started on port 80001")
+    console.log("server started on port 8001")
 })
