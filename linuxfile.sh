9f5af35d425a371af386a6519e49bf94c9202a99
lokanath1234 <lokanathsahoolk@gmail.com>
2023-03-17 19:09:09 +0530

    new test

--- linuxfile.sh
+++ linuxfile.sh
@@ -6,3 +6,7 @@ MY_AGE=23
 echo Hello there my name is $MY_NAME and my age is $MY_AGE
 
 echo this is the file name $0
+
+echo this is the date $(date)
+
+echo THIS IS THE EXIT CODE RETURNED FROM THE SCRIPT $?
